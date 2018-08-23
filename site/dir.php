048a124df1ece11182aa811cfa6be0bd8b2f5bdf
Alan <alanlyra7@gmail.com>
2018-08-23 01:53:12 -0300

    Mitanca do Herbert

---
+++ site/dir.php
@@ -0,0 +1,22 @@
+
+<?php
+	function scan_dir($dir) {
+	    $ignored = array('.', '..', '.svn', '.htaccess');
+
+	    $files = array();
+	    foreach (scandir($dir) as $file) {
+	        if (in_array($file, $ignored)) continue;
+	        $files[$file] = filemtime($dir . '/' . $file);
+	    }
+
+	    arsort($files);
+	    $files = array_keys($files);
+
+	    return ($files) ? $files : false;
+	}
+
+	if (isset($_POST['callFunc1'])) {
+      echo scan_dir($_POST['callFunc1'])[0];
+  }
+
+?>
